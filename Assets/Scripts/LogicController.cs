da82076581af3134f8c246cd3f4a5e7949032187
EntranceJew <EntranceJew@gmail.com>
2015-06-22 20:46:48 -0400

    Added test fish scene to spawn objects within a region.

---
+++ Assets/Scripts/LogicController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LogicController : MonoBehaviour {
+	public GameObject thingToSpawn;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		UpdateControls ();
+	}
+
+	void UpdateControls(){
+		if (Input.GetButtonDown ("Fire1")) {
+			Vector3 mousePos = Input.mousePosition;
+			mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+			// push the 
+			mousePos.z = 0.0f;
+
+			SpawnThingAt(mousePos);
+		}
+	}
+
+	GameObject SpawnThingAt(Vector3 placeToSpawn){
+		GameObject newThing = (GameObject) Instantiate (
+			thingToSpawn,
+			placeToSpawn,
+			Quaternion.identity
+		);
+
+		return newThing;
+	}
+}
---
+++ Assets/Scripts/SpawnArea.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnArea : MonoBehaviour {
+	public GameObject spawnObject;
+	public float rateOfSpawn;
+	private float nextSpawn;
+
+	// Use this for initialization
+	void Start () {
+		nextSpawn = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time > nextSpawn) {
+			nextSpawn = Time.time + rateOfSpawn;
+			SpawnThis ();
+		}
+	}
+
+	public Object SpawnThis(){
+		Vector3 position = getRandomPosition ();
+		return Instantiate (spawnObject, position, transform.rotation);
+	}
+
+	public Vector3 getRandomPosition(){
+		Vector3 spawnBox = gameObject.transform.localScale;
+		Vector3 spawnPos = gameObject.transform.position;
+		Vector3 newPos = new Vector3 (
+			Random.Range (-spawnBox.x / 2, spawnBox.x / 2),
+			Random.Range (spawnPos.y, spawnBox.y + spawnPos.y / 2 ),
+			0.0f
+		);
+		Debug.Log (newPos);
+		return newPos;
+	}
+}
