191001ea162ff48a1cdd91c2c7c91e3785fbc36a
EntranceJew <EntranceJew@gmail.com>
2015-06-24 00:40:24 -0400

    Made less broken dragging, maybe.

--- Assets/BurgerObserver.cs
+++ Assets/BurgerObserver.cs
@@ -2,38 +2,7 @@
 using System.Collections;
 
 public class BurgerObserver : MonoBehaviour {
-	public Vector3 lastPosition;
 
-	// Use this for initialization
-	void Start () {
-		lastPosition = Vector3.up;
-	}
 	
-	// Update is called once per frame
-	void Update () {
-		Debug.DrawRay(Camera.main.transform.position, Vector3.up);
-
-		if (Input.GetButtonDown ("Fire1")) {
-			Vector3 mousePos = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane);
-			Vector3 point = Camera.main.ScreenToWorldPoint (mousePos);
-			
-			
-			
-			//Ray rayToCameraPos = new Ray(Camera.main.transform.position, point);
-			RaycastHit hit;
-			bool didIt = Physics.Raycast (Camera.main.transform.position, Camera.main.transform.position - mousePos, out hit, 1000);
-			if (didIt) {
-				Debug.Log (hit.collider.name + ", " + hit.collider.tag);
-			} else {
-				Debug.Log ("HORSEHOCKEY");
-				Debug.Log (Camera.main.transform.position);
-				Debug.Log ("YUKKOMUKKO");
-				Debug.Log (mousePos);
-				Debug.Log ("SPAGHET");
-				Debug.Log (point);
-				Debug.Log ("WATASHIWA");
-				Debug.Log (Camera.main.transform.position - point);
-			}
-		}
-	}
+	// This drag wasn't like the OP wanted. function OldDrag() { var ray : Ray = Camera.main.ScreenPointToRay(Input.mousePosition); grabbed.position = ray.origin + ray.direction * grabDistance; } 
 }
---
+++ Assets/Draggable.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// from: http://answers.unity3d.com/questions/55406/picking-dragging-objects-with-mouse.html
+// and by from I mean EntranceJew had to pick apart that ancient mess AND translate it into C#
+// AND figure out what the hell went wrong in the formatting that means "ITALICIZE"
+public class Draggable : MonoBehaviour {
+	public Transform grabbed;
+	public float grabDistance = 10.0f;
+	
+	public bool useToggleDrag; // Didn't know which style you prefer.
+	
+	void Update () {
+		if (useToggleDrag) {
+			UpdateToggleDrag ();
+		} else {
+			UpdateHoldDrag (); 
+		}
+	}
+	
+	// Toggles drag with mouse click 
+	void UpdateToggleDrag () {
+		if (Input.GetButtonDown ("Fire1")) {
+			Grab ();
+		} else if (grabbed) {
+			Drag (); 
+		}
+	}
+	
+	// Drags when user holds down button function 
+	void UpdateHoldDrag () {
+		if (Input.GetButtonDown("Fire1")){
+			if (grabbed) {
+				Drag();
+			} else {
+				Grab();
+				grabbed = null; 
+			}
+		}
+	}
+	
+	void Grab() {
+		if (grabbed) {
+			grabbed = null;
+		} else { 
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			if (Physics.Raycast (ray, out hit)) {
+				grabbed = hit.transform; 
+			}
+		}
+	}
+	
+	void Drag() {
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		Vector3 position = transform.position + transform.forward / grabDistance;
+		Plane plane = new Plane(-transform.forward, position);
+		float distance;
+		if (plane.Raycast(ray, out distance)) {
+			grabbed.position = ray.origin + ray.direction / distance;
+			grabbed.rotation = transform.rotation; 
+		} 
+	}
+}
