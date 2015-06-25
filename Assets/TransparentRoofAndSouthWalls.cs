cb2ee4a5ef50d3299dd547d213bc19701852e751
saforer <saforer@gmail.com>
2015-06-25 10:27:25 -0400

    Made south walls and roof transparent when you leave their insides and not when they aren't

---
+++ Assets/TransparentRoofAndSouthWalls.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TransparentRoofAndSouthWalls : MonoBehaviour {
+	public List<GameObject> toTransparentList = new List<GameObject>();
+
+
+	void OnTriggerEnter (Collider col) {
+		if (col.name == "Player") {
+			//Make stuff transparent
+			foreach (GameObject obj in toTransparentList) {
+				obj.GetComponent<MeshRenderer> ().enabled = false;
+			}
+		}
+	}
+
+	void OnTriggerExit (Collider col) {
+		if (col.name == "Player") {
+			//Make stuff NOT transparent again
+			foreach (GameObject obj in toTransparentList) {
+				obj.GetComponent<MeshRenderer> ().enabled = true;
+			}
+		}
+	}
+}
