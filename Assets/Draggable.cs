660b198b74c2b448a40d7b11bb74835675aafbc2
EntranceJew <EntranceJew@gmail.com>
2015-06-25 23:25:33 -0400

    Fixed dragging of items in BurgerGame.
    Added a score of materials for colorizing things.
    Added some functions to draw Debug objects.

--- Assets/Draggable.cs
+++ Assets/Draggable.cs
@@ -5,10 +5,21 @@ using System.Collections;
 // and by from I mean EntranceJew had to pick apart that ancient mess AND translate it into C#
 // AND figure out what the hell went wrong in the formatting that means "ITALICIZE"
 public class Draggable : MonoBehaviour {
-	public Transform grabbed;
+	public GameObject grabbedObject;
 	public float grabDistance = 10.0f;
+
+	public GameObject debugPoint;
+	public GameObject debugArrow;
 	
 	public bool useToggleDrag; // Didn't know which style you prefer.
+
+	public Ray ray;
+	public Vector3 position;
+	public Plane plane;
+	public float distance;
+	public Vector3 temp;
+
+	public RaycastHit hit;
 	
 	void Update () {
 		if (useToggleDrag) {
@@ -22,7 +33,7 @@ public class Draggable : MonoBehaviour {
 	void UpdateToggleDrag () {
 		if (Input.GetButtonDown ("Fire1")) {
 			Grab ();
-		} else if (grabbed) {
+		} else if (grabbedObject != null) {
 			Drag (); 
 		}
 	}
@@ -40,26 +51,40 @@ public class Draggable : MonoBehaviour {
 	}*/
 	
 	void Grab() {
-		if (grabbed) {
-			grabbed = null;
-		} else { 
-			RaycastHit hit;
-			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		if (grabbedObject != null) {
+			grabbedObject = null;
+		} else {
+			ray = Camera.main.ScreenPointToRay (Input.mousePosition);
 			if (Physics.Raycast (ray, out hit)) {
-				grabbed = hit.transform; 
+				grabbedObject = hit.transform.gameObject;
+				//DebugPoint(hit.transform, "GrabRaycast");
 			}
 		}
 	}
 	
 	void Drag() {
-		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-		Vector3 position = transform.position + transform.forward;
-		Plane plane = new Plane(-transform.forward, position);
-		float distance;
+		ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		plane = new Plane(-transform.forward, position);
 		if (plane.Raycast(ray, out distance)) {
-			Vector3 temp = ray.origin + ray.direction / distance;
-			grabbed.position = temp;
-			grabbed.rotation = transform.rotation; 
-		} 
+			temp = ray.origin + ray.direction * distance;
+			grabbedObject.transform.position = temp;
+			//grabbedObject.transform.rotation = transform.rotation;
+		}
+	}
+
+	void DebugPoint(Vector3 position, string identifier){
+		GameObject go = (GameObject) Instantiate (debugPoint, position, Quaternion.identity);
+		go.name = "DebugPoint "+identifier+" @ "+Time.time;
+	}
+
+	void DebugPoint(Transform transform, string identifier){
+		GameObject go = (GameObject) Instantiate (debugPoint, transform.position, transform.rotation);
+		go.name = "DebugPoint "+identifier+" @ "+Time.time;
+	}
+	
+	void DebugArrow(Vector3 position, Vector3 direction, string identifier){
+		Vector3 newPos = position + direction;
+		GameObject go = (GameObject) Instantiate (debugArrow, position, new Quaternion(newPos.x, newPos.y, newPos.z, 0.0f));
+		go.name = "DebugArrow "+identifier+" @ "+Time.time;
 	}
 }
