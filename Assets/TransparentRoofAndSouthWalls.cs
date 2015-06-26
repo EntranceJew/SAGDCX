44dc511e101b196844ad9a65a0f608065e3484f4
EntranceJew <EntranceJew@gmail.com>
2015-06-25 20:07:26 -0400

    Made TransparentRoofAndSouthWalls have the ability to hide any arbitrary structure it needs (such as any floors above it).

--- Assets/TransparentRoofAndSouthWalls.cs
+++ Assets/TransparentRoofAndSouthWalls.cs
@@ -4,22 +4,43 @@ using System.Collections.Generic;
 
 public class TransparentRoofAndSouthWalls : MonoBehaviour {
 	public List<GameObject> toTransparentList = new List<GameObject>();
+	public List<GameObject> toTransparentToppers = new List<GameObject>();
 
 
-	void OnTriggerEnter (Collider col) {
+	public void OnTriggerEnter (Collider col) {
 		if (col.name == "Player") {
 			//Make stuff transparent
-			foreach (GameObject obj in toTransparentList) {
-				obj.GetComponent<MeshRenderer> ().enabled = false;
+			HideEverything();
+		}
+	}
+
+	public void OnTriggerExit (Collider col) {
+		if (col.name == "Player") {
+			//Make stuff NOT transparent again
+			UnhideEverything();
+		}
+	}
+
+	public void HideEverything(){
+		foreach (GameObject obj in toTransparentList) {
+			obj.GetComponent<MeshRenderer> ().enabled = false;
+		}
+
+		foreach (GameObject obj in toTransparentToppers) {
+			foreach(MeshRenderer mesh in obj.GetComponentsInChildren<MeshRenderer>()){
+				mesh.enabled = false;
 			}
 		}
 	}
 
-	void OnTriggerExit (Collider col) {
-		if (col.name == "Player") {
-			//Make stuff NOT transparent again
-			foreach (GameObject obj in toTransparentList) {
-				obj.GetComponent<MeshRenderer> ().enabled = true;
+	public void UnhideEverything(){
+		foreach (GameObject obj in toTransparentList) {
+			obj.GetComponent<MeshRenderer> ().enabled = true;
+		}
+
+		foreach (GameObject obj in toTransparentToppers) {
+			foreach(MeshRenderer mesh in obj.GetComponentsInChildren<MeshRenderer>()){
+				mesh.enabled = true;
 			}
 		}
 	}
