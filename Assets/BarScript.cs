76f8977d8f9176dd62f6238e0b4dc26476418ee5
saforer <saforer@gmail.com>
2015-06-25 20:26:09 -0400

    Made a home spun bargraph, ghetto as all hell but it works as a simple demo and can now be prettified?
    Signed-off-by: saforer <saforer@gmail.com>

---
+++ Assets/BarScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BarScript : MonoBehaviour {
+
+	public GameObject bar;
+
+	public List<float> dataList = new List<float>();
+
+	// Use this for initialization
+	void Start () {
+		int temp = 0;
+		foreach (int data in dataList) {
+			float spacing = 15f;
+			Vector3 pos = gameObject.transform.position;
+			pos.x += (temp * spacing);
+			pos.y += (dataList[temp] / 2) * 100;
+			GameObject graphedBar = Instantiate(bar, pos, Quaternion.identity) as GameObject;
+			graphedBar.transform.SetParent(gameObject.transform);
+            graphedBar.GetComponent<RectTransform>().sizeDelta = new Vector2 (100, 100 * dataList[temp]);
+			temp++;
+		}
+	}
+}
