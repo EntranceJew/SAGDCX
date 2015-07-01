fc83b30ba04b4de0318b02c9b210312ace4f7978
saforer <saforer@gmail.com>
2015-07-01 16:18:26 -0400

    Made rotation speed a variable, and made it adjustable in the editor.
    Signed-off-by: saforer <saforer@gmail.com>

--- Assets/Scripts/RotateScript.cs
+++ Assets/Scripts/RotateScript.cs
@@ -2,9 +2,11 @@
 using System.Collections;
 
 public class RotateScript : MonoBehaviour {
+	public float rotationSpeed = 1;
+
 
 	// Update is called once per frame
 	void Update () {
-		transform.Rotate (0, 1, 0);
+		transform.Rotate (0, rotationSpeed, 0);
 	}
 }
