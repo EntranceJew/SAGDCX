e6043612068da0f886c46ea507c9010f0a17dc33
EntranceJew <EntranceJew@gmail.com>
2015-07-01 22:20:13 -0400

    Made it so that stacking things parents all burg parts into the burg zone.

---
+++ Assets/BurgBuilder.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BurgBuilder : MonoBehaviour {
+	public PartZone pz;
+
+	// Use this for initialization
+	void Start () {
+		pz = gameObject.GetComponentInChildren<PartZone> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ObtainNewPart(GameObject part){
+		// We only accept orphans.
+		// Originally we only cared if the object's parent wasn't us but that's more effort,
+		// because the first condition clashes with the second half and boy am I tired.
+		// part.transform.parent != null & part.transform.parent.gameObject != this.gameObject
+		if (part.transform.parent == null 
+		    || (part.transform.parent != null && part.transform.parent != this.transform)) {
+			Debug.Log ("I just found myself a new " + part.name);
+			part.transform.parent = this.gameObject.transform;
+
+			Vector3 pzt = pz.transform.localPosition;
+
+			pzt.y += 0.7f;
+
+			pz.transform.localPosition = pzt;
+
+			Rigidbody rb = part.GetComponent<Rigidbody> ();
+			rb.useGravity = false;
+			rb.isKinematic = true;
+			rb.freezeRotation = true;
+		}
+	}
+}
---
+++ Assets/PartZone.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PartZone : MonoBehaviour {
+	public BurgBuilder bb;
+
+	// Use this for initialization
+	void Start () {
+		bb = transform.parent.GetComponent<BurgBuilder> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider col){
+		if (col.gameObject.tag == "Food") {
+			Debug.Log (col.gameObject);
+			bb.ObtainNewPart(col.gameObject);
+		}
+	}
+}
