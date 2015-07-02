cde76c226a1d3e0aa9bfe04901ae5c636b78f177
EntranceJew <EntranceJew@gmail.com>
2015-07-01 20:38:45 -0400

    Destroyed BurgerObserver, no longer needed.

--- Assets/Scripts/BurgerObserver.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class BurgerObserver : MonoBehaviour {
-
-	
-	// This drag wasn't like the OP wanted. function OldDrag() { var ray : Ray = Camera.main.ScreenPointToRay(Input.mousePosition); grabbed.position = ray.origin + ray.direction * grabDistance; } 
-}
