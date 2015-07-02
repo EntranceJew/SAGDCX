5717bafd01871c57afe4813cf906ec34b071179c
EntranceJew <EntranceJew@gmail.com>
2015-07-01 20:39:51 -0400

    Added PlateThing to act as a spawn point for PlateMachine.

---
+++ Assets/PlateMachine.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlateMachine : MonoBehaviour {
+	public List<GameObject> spawnList;
+
+	private int listPos;
+
+	// Use this for initialization
+	void Start () {
+		listPos = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		foreach(PlateThing plate in this.gameObject.GetComponentsInChildren<PlateThing>()){
+			if(plate.SpawnThing(spawnList[listPos])){
+				listPos += 1;
+				if(listPos >= spawnList.Count){
+					listPos = 0;
+				}
+			}
+		}
+	}
+}
---
+++ Assets/PlateThing.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlateThing : MonoBehaviour {
+	public bool canSpawnThing;
+	public GameObject spawnedThing;
+
+	public float spawnDelay = 9.0f;
+	public float lastSpawnTime = Mathf.NegativeInfinity;
+
+	public float timeExpected;
+	public float timePreview;
+
+
+	// Use this for initialization
+	void Start () {
+		canSpawnThing = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeExpected = Time.time + spawnDelay;
+		timePreview = Time.time;
+	}
+
+	void OnTriggerExit(Collider col){
+		if (col.gameObject == spawnedThing) {
+			canSpawnThing = true;
+		}
+	}
+
+	public bool SpawnThing(GameObject thingToSpawn){
+		if (lastSpawnTime + spawnDelay < Time.time && canSpawnThing) {
+			Transform spawnPoint = transform.Find ("PlateSpawn");
+			spawnedThing = (GameObject)Instantiate (thingToSpawn, spawnPoint.position, spawnPoint.rotation);
+			lastSpawnTime = Time.time;
+			canSpawnThing = false;
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
