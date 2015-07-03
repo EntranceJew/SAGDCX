86272ffcb4d64017845dfccccb4d60788e91afa0
saforer <saforer@gmail.com>
2015-07-03 16:19:44 -0400

    UI rendered onto clipboard through a canvas.
    WOW NEAT SHIT!
    
    Signed-off-by: saforer <saforer@gmail.com>

---
+++ Assets/DispenseLiquid.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DispenseLiquid : MonoBehaviour {
+	public string resourceName;
+	static GameObject liquidToDispense;
+	public GameObject pos;
+
+
+	// Use this for initialization
+	void Start () {
+		liquidToDispense = Resources.Load (resourceName) as GameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Instantiate(liquidToDispense, pos.transform.position, Quaternion.identity);
+	}
+}
