2e49b7f743e8992d252cca1fe79c57b53f5a3e4c
EntranceJew <EntranceJew@gmail.com>
2015-07-03 14:41:15 -0400

    Made Lightbulb a prefab.
    Added tags for maincanvas and burgbuilder.
    Moved GetOrder to Lightbulb.
    Re-enabled PartZone script.

--- Assets/Scripts/GetOrder.cs
+++ Assets/Scripts/GetOrder.cs
@@ -3,18 +3,22 @@ using UnityEngine.UI;
 using System.Collections;
 
 public class GetOrder : MonoBehaviour {
+
+	GameObject canvas;
 	Text instruction;
 	Order order;
 
 	public bool rand;
 
 	void Start () {
+		canvas = GameObject.FindGameObjectWithTag("MainCanvas");
+
 		if (rand) {
 			order = new Order ();
 		} else {
 			order = new Order ("Normal");
 		}
-		instruction = GetComponent<Text> ();
+		instruction = canvas.transform.Find("OrderText").gameObject.GetComponent<Text> ();
 		instruction.text = "Your order is: " + order.toString ();
 	}
 }
