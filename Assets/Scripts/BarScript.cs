03c2f26a32f3aeb91aae5c20872396f1729e4ced
saforer <saforer@gmail.com>
2015-07-03 18:20:01 -0400

    Bar Graph & Other Crap HOORAY!
    Signed-off-by: saforer <saforer@gmail.com>

--- Assets/Scripts/BarScript.cs
+++ Assets/Scripts/BarScript.cs
@@ -12,13 +12,30 @@ public class BarScript : MonoBehaviour {
 	void Start () {
 		int temp = 0;
 		foreach (int data in dataList) {
-			float spacing = 15f;
-			Vector3 pos = gameObject.transform.position;
-			pos.x += (temp * spacing);
-			pos.y += (dataList[temp] / 2) * 100;
-			GameObject graphedBar = Instantiate(bar, pos, Quaternion.identity) as GameObject;
-			graphedBar.transform.SetParent(gameObject.transform);
-            graphedBar.GetComponent<RectTransform>().sizeDelta = new Vector2 (100, 100 * dataList[temp]);
+			//Position of start element.
+			Vector3 pos = Vector3.zero;
+			pos.x += temp * 1.1f;
+			pos.y -= data * .5f;
+
+			//Draw bar
+			GameObject graphedBar = Instantiate(bar, Vector3.zero, Quaternion.identity) as GameObject;
+			RectTransform rt = graphedBar.GetComponent<RectTransform>();
+
+			//Set Parent
+			rt.SetParent(gameObject.transform);
+
+			//Set Height
+			rt.sizeDelta = new Vector2( 1f, data);
+
+			//Set Position
+			rt.localPosition = pos;
+
+			//Set Rotation
+			rt.localRotation = Quaternion.identity;
+
+			//Set scale
+			rt.localScale = Vector3.one;
+
 			temp++;
 		}
 	}
