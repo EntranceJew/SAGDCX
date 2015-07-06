95bfc62671342b970cab5570798a855e2e848fa2
saforer <saforer@gmail.com>
2015-07-05 21:08:50 -0400

    Aaaaaaaaaaaaa Ok the orders should now be created as an example.
    Signed-off-by: saforer <saforer@gmail.com>

--- Assets/Scripts/GetOrder.cs
+++ Assets/Scripts/GetOrder.cs
@@ -3,16 +3,39 @@ using UnityEngine.UI;
 using System.Collections;
 
 public class GetOrder : MonoBehaviour {
-
+	
 	GameObject canvas;
 	Text instruction;
 	Order order;
-
+	public GameObject example;
+	public float upVar;
+	
 	public bool rand;
-
+	
+	
+	//INGREDIENTS!
+	public GameObject BunBottom;
+	public GameObject Patty;
+	public GameObject Cheese;
+	public GameObject Lettuce;
+	public GameObject Tomato;
+	public GameObject Onion;
+	public GameObject Pickles;
+	public GameObject BunTop;
+	public GameObject Rat;
+	
+	
 	void Start () {
+		//Get the order
+		OrderCreation ();
+		
+		//Make the order appear in the box
+		MakeOrderExample ();
+	}
+	
+	void OrderCreation() {
 		canvas = GameObject.FindGameObjectWithTag("MainCanvas");
-
+		
 		if (rand) {
 			order = new Order ();
 		} else {
@@ -21,8 +44,58 @@ public class GetOrder : MonoBehaviour {
 		instruction = canvas.transform.Find("OrderText").gameObject.GetComponent<Text> ();
 		instruction.text = "Your order is: " + order.toString ();
 	}
-
-	void Update (){
-
+	
+	void MakeOrderExample() {
+		//Clean out previous order
+		TrashLastOrder ();
+		
+		
+		//Take order
+		int ingredient = 0;
+		while (ingredient <= order.completeOrder.Count) {
+			//Look at ingredient
+			GameObject ing = FindGameObjectForIngredient(order.completeOrder[ingredient]);
+			//Find where to drop it.
+			Vector3 finalPos = example.transform.position;
+			finalPos.y += ingredient * upVar;
+			//Instantiate ingredient
+			GameObject addedIngredient = Instantiate(ing, finalPos, Quaternion.identity) as GameObject;
+			addedIngredient.transform.SetParent(example.transform);
+			addedIngredient.transform.localRotation = Quaternion.Euler(-90,0,0);
+			//Repeat
+			ingredient++;
+		}
+	}
+	
+	GameObject FindGameObjectForIngredient(string prefabName) {
+		switch (prefabName) {
+		case "BunBottom":
+			return BunBottom;
+		case "Patty":
+			return Patty;
+		case "Cheese":
+			return Cheese;
+		case "Lettuce":
+			return Lettuce;
+		case "Tomato":
+			return Tomato;
+		case "Onion": 
+			return Onion;
+		case "Pickles":
+			return Pickles;
+		case "BunTop":
+			return BunTop;
+		default:
+			return Rat;
+		}
+	}
+	
+	void TrashLastOrder() {
+		foreach (Transform child in example.transform) {
+			if (child != example.transform) {
+				//Ok good we aren't deleting the actual example burger gameobject container
+				Destroy (child);
+			}
+		}
 	}
 }
