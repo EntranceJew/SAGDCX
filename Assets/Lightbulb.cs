a020484c7b44a00be90f4e43ded3168df2af5008
EntranceJew <EntranceJew@gmail.com>
2015-07-06 18:45:21 -0400

    Added Lightbulb script to lightbulb, lights up and unlights.

---
+++ Assets/Lightbulb.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lightbulb : MonoBehaviour {
+	public Material litUp;
+	public Material unLit;
+	public Renderer rend;
+
+	// Use this for initialization
+	void Start () {
+		rend = GetComponent<Renderer>();
+		rend.enabled = true;
+		UnLight ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//debug material switching
+		//if (Time.time > 5.0f) {
+		//	LightUp ();
+		//}
+	}
+
+	public void LightUp(){
+		rend.sharedMaterial = litUp;
+	}
+
+	public void UnLight(){
+		rend.sharedMaterial = unLit;
+	}
+}
