a8e3658882ebe15f415e95407eda2c046e42d6c9
saforer <saforer@gmail.com>
2015-07-07 14:53:07 -0400

    made it so clicking the clip of the clipboard lerps the position and the rotation to the cameras.

---
+++ Assets/MoveToCameraOnClick.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveToCameraOnClick : MonoBehaviour {
+	public Transform ownPosition;
+	public Transform cameraPosition;
+	public bool wall = true;
+	public float speed;
+	float percent = 1;
+	Transform goTo;
+	Transform from;
+
+	// Update is called once per frame
+	void Update () {
+		Move ();
+	}
+
+	void OnMouseDown() {
+		wall = !wall;
+		percent = 0;
+	}
+
+	void Move() {
+		percent += Time.deltaTime * speed;
+
+
+
+		if (wall) {
+			goTo = ownPosition;
+			from = cameraPosition;
+		} else {
+			goTo = cameraPosition;
+			from = ownPosition;
+		}
+
+		transform.position = Vector3.Lerp (from.position, goTo.position, percent);
+		transform.rotation = Quaternion.Lerp (from.rotation, goTo.rotation, percent);
+
+		if (percent > 1) {
+			percent = 1;
+		}
+	}
+}
