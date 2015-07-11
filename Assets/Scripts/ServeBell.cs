c4a95591089ffafca71a12d56f520f72cd33aa03
EntranceJew <EntranceJew@gmail.com>
2015-07-10 21:14:18 -0400

    Made the ServeBell different from the regular ScoreBell.

---
+++ Assets/Scripts/ServeBell.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ServeBell : MonoBehaviour {
+	public GameObject lb;
+	public GameObject burgBuild;
+
+	public bool evaluated = false;
+	public bool isEvaluating = false;
+
+	private AltScore score;
+
+	void Start(){
+		score = GetComponent<AltScore> ();
+	}
+
+	void OnMouseDown() {
+		//Check order : THIS SHOULD BE REPLACED WITH ACTUAL CHECK ORDER SCRIPTS, this is just for human evaluation right now.
+		AudioSource audio = GetComponent<AudioSource> ();
+		audio.Play ();
+
+		lb.GetComponent<AltGetOrder> ().NewOrder ();
+	}
+
+	void TrashBurger(){
+		foreach (Transform t in burgBuild.transform) {
+			if (t.name != "PartZone") {
+				Destroy (t.gameObject);
+			}
+		}
+		
+		//Allow new burg to be made
+		foreach (Transform t in burgBuild.transform) {
+			if (t.name == "PartZone") {
+				t.GetComponent<PartZone> ().enabled = true;
+			}
+		}
+	}
+
+}
