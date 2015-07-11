1e742fe9f433a624e2507e2738eb02feee1b798a
EntranceJew <EntranceJew@gmail.com>
2015-07-10 20:40:16 -0400

    Added DayValues thing to track the values in the day.

---
+++ Assets/DayValues.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class PlayerValues {
+	public string name;
+	
+	// range 0 to 1;
+	public int dayNumber;
+
+	public float cash;
+
+	public InventoryItems[] stock;
+}
+
+[System.Serializable]
+public class DayValue {
+	public string name;
+
+	// range 0 to 1;
+	public float mistakeTolerance;
+
+	public InventoryItems[] shipments;
+	// projected demands
+}
+
+public class DayValues : MonoBehaviour {
+	public List<DayValue> values; // = new List<DayValue>();
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
