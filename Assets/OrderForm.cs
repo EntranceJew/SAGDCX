b5abf74234b16025d4f8381fd25246358edd62ac
EntranceJew <EntranceJew@gmail.com>
2015-07-13 12:44:03 -0400

    Added RowManager script to act on each clipboard row.
    Added OrderForm to manage the whole form.

---
+++ Assets/OrderForm.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OrderForm : MonoBehaviour {
+	
+	private RowManager[] rows;
+
+	// Use this for initialization
+	void Start () {
+		rows = gameObject.GetComponentsInChildren<RowManager> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// form buttons
+	public void Confirm(){
+		Debug.Log ("PURCHASED A BUNCH OF STUFF FOR $"+GetTotal().ToString());
+		Clear ();
+	}
+
+	public void Clear(){
+		foreach (RowManager row in rows) {
+			row.quantity = 0;
+		}
+	}
+
+	// helpers
+	public float GetTotal(){
+		float total = 0.0f;
+		foreach (RowManager row in rows) {
+			total += row.quantity*row.rate;
+		}
+		return total;
+	}
+}
---
+++ Assets/RowManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class RowManager : MonoBehaviour {
+	public string itemName;
+
+	public int quantity;
+	public float rate;
+	public int stock;
+	public GameObject represents;
+
+	private Text itemText;
+	private Text quantText;
+	private Text totalText;
+	private Text stockText;
+
+	// Use this for initialization
+	void Start () {
+		itemText = transform.Find ("ItemName").gameObject.GetComponent<Text>();
+		quantText = transform.Find ("Quantity").gameObject.GetComponent<Text>();
+		totalText = transform.Find ("Total").gameObject.GetComponent<Text>();
+		stockText = transform.Find ("Remaining").gameObject.GetComponent<Text>();
+
+		SetText ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		SetText ();
+	}
+
+	public void SetText(){
+		itemText.text = itemName;
+		quantText.text = quantity.ToString();
+		totalText.text = "$"+(quantity * rate).ToString ("F2");
+		stockText.text = "( "+stock.ToString()+" )";
+	}
+
+	public void Add(){
+		quantity++;
+	}
+
+	public void Subtract(){
+		quantity--;
+	}
+}
