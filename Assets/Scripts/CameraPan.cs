02ff4e89d363d513cf18d6a62398e79d9850911c
TomRintjema <tomrintjema@hotmail.com>
2015-07-17 09:20:54 -0400

    Made a dining area for burger judgement.

---
+++ Assets/Scripts/CameraPan.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraPan : MonoBehaviour {
+	public float MinAngle = -45f;
+	public float MaxAngle = 45f;
+	public float Speed = 1;
+	private int Direction = 1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(Vector3.right * Speed * Direction * Time.deltaTime);
+		if (transform.eulerAngles.x < MinAngle) {
+			Direction = 1;
+		}
+		if (transform.eulerAngles.x > MaxAngle) {
+			Direction = -1;
+		}
+
+	}
+}
