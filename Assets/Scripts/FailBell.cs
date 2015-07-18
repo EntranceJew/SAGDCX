223f4454363d7ee7d21a43df190cc312fc666253
EntranceJew <EntranceJew@gmail.com>
2015-07-18 01:37:47 -0400

    Added FailBell to trigger day failure for debugging purposes.

--- Assets/Scripts/FailBell.cs
+++ Assets/Scripts/FailBell.cs
@@ -4,24 +4,12 @@ using System.Collections.Generic;
 
 public class FailBell : MonoBehaviour {
 	public int pressState;
-
-	public PlayerValues playerValues;
 	public DayManager dayManager;
-
-	void Start(){
-		pressState = 0;
-
-		playerValues.Save ();
-	}
 
 	void OnMouseDown() {
 		AudioSource audio = GetComponent<AudioSource> ();
 		audio.Play ();
 
-		FuckEverythingUp ();
-	}
-
-	public void FuckEverythingUp(){
 		dayManager.FailDay();
 		Debug.Log ("YOU ARE DEAD. NOT BIG SURPRISE.");
 	}
