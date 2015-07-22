8e6333a531d7cba442e745dfdb3e70153f5d4d4b
EntranceJew <EntranceJew@gmail.com>
2015-07-21 23:17:11 -0400

    Made the BurgJudgeCatcher dish out a money value for a scored burger.

--- Assets/BurgJudgeCatcher.cs
+++ Assets/BurgJudgeCatcher.cs
@@ -1,15 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class BurgJudgeCatcher : MonoBehaviour {
 	public BurgBuilder burgBuilder;
 	public AltGetOrder getOrder;
 	public AltScore score;
 	public PlayerValues playerValues;
+	public DayValues dayValues;
+	public Text moneyText;
+
+	public Color gainMoney = new Color( 83.0f/255.0f, 255.0f/255.0f, 77.0f/255.0f);
+	public Color loseMoney = new Color(255.0f/255.0f,  77.0f/255.0f, 77.0f/255.0f);
 
 	// Use this for initialization
 	void Start () {
-	
+		moneyText.text = "";
 	}
 	
 	// Update is called once per frame
@@ -23,10 +29,35 @@ public class BurgJudgeCatcher : MonoBehaviour {
 			// @TODO: Display scores on the TV, time it out. You know, do what the people like.
 			score.EvaluateBurger(getOrder.CurrentOrder().completeOrder, burgBuilder.GetChildParts());
 			playerValues.AddScores(score.GetAchievedScore(), score.GetMaxScore());
+			GetDollarDollarBillsYall();
 			// get down from there you rapscallion
 			burgBuilder.UnHoist();
 		} else {
 			Debug.Log ("WHAT TO HECK: " + col.gameObject.name);
+		}
+	}
+
+	void GetDollarDollarBillsYall(){
+		float satisfaction = (float)score.GetAchievedScore () / (float)score.GetMaxScore ();
+
+		RMenuItem mitem = getOrder.lastMenuItem;
+
+		if (satisfaction > dayValues.GetTodaysMistakeTolerance ()) {
+			Debug.Log ("SATISFACTION: I LIKED IT!");
+			Debug.Log (mitem.value);
+			Debug.Log (mitem.price);
+			playerValues.Earn (mitem.value * satisfaction);
+			moneyText.color = gainMoney;
+			moneyText.text = "+$"+(mitem.value*satisfaction).ToString("F2");
+		} else {
+			Debug.Log ("SATISFACTION: I FUCKING HATED IT!");
+			Debug.Log ("VALUE: "+mitem.value);
+			Debug.Log ("PRICE: "+mitem.price);
+			Debug.Log ("SATISFACTION: "+satisfaction);
+			Debug.Log ("FUCKED UP: "+(mitem.value * (1.0f-satisfaction)));
+			playerValues.Spend (mitem.value * (1.0f-satisfaction));
+			moneyText.color = loseMoney;
+			moneyText.text = "-$"+(mitem.value * (1.0f-satisfaction)).ToString("F2");
 		}
 	}
 }
