b2708ea0ae32b3057fb3292312691ec36c3736d9
saforer <saforer@gmail.com>
2015-07-22 20:24:22 -0400

    Made an attract mode.
    Signed-off-by: saforer <saforer@gmail.com>

---
+++ Assets/AttractModeBurgSpawn.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AttractModeBurgSpawn : MonoBehaviour {
+	public FoodLookup fl;
+	List<GameObject> currentBurg = new List<GameObject>();
+	float count = 0;
+	public float maxTime = 7;
+
+	void Update() {
+		count += Time.deltaTime;
+		//If there's no burger on screen YA GOTTA MAKE A BURGER
+		if (currentBurg.Count == 0) {
+			count = 0;
+			float upVar = .5f;
+			int numToSpawn = Random.Range (5,11);
+
+			for (int i = 0; i < numToSpawn; i++) {
+				int foodToSpawn = Random.Range (0, fl.lookup.Count);
+				Vector3 burgSpawnPosition = transform.position;
+				burgSpawnPosition.y += i * upVar;
+				GameObject food = Instantiate(fl.lookup[foodToSpawn], burgSpawnPosition, Quaternion.Euler(new Vector3(0,Random.Range(0,360),0))) as GameObject;
+				Rigidbody rb = food.GetComponent<Rigidbody>();
+				rb.freezeRotation = false;
+				rb.mass = 50;
+				rb.drag = 1;
+				currentBurg.Add(food);
+			}
+		}
+
+		//Ok they stared at this burger enough time to give them a new one
+		if (count > maxTime) {
+			foreach (GameObject obj in currentBurg) {
+				Destroy(obj);
+			}
+
+			currentBurg.Clear ();
+		}
+	}
+}
