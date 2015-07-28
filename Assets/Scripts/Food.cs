e0fd2e9f3f70ab1f9441caaf3f36db7fb138585c
EntranceJew <EntranceJew@gmail.com>
2015-07-28 18:59:34 -0400

    Made PartZone use similar foodpope logic fod delayed collision detection.
    Made Food highlight if it is FoodPope.

--- Assets/Scripts/Food.cs
+++ Assets/Scripts/Food.cs
@@ -115,6 +115,24 @@ public class Food : MonoBehaviour {
 		bb = newBurgBuilder;
 		gameObject.transform.parent = bb.gameObject.transform;
 		this.isFoodPope = true;
+		/* disable the following if we're not in the future */
+		BecomeLilMac ();
+	}
+
+	public void BecomeLilMac(){
+		Material lilMat = Resources.Load("ResHighlight", typeof(Material)) as Material;
+		List<Renderer> renderers = new List<Renderer> (GetComponents<Renderer> ());
+		renderers.AddRange (gameObject.GetComponentsInChildren<Renderer> ());
+		// ALL THE RENDERERS GET TICKLED, ALL OF 'EM.
+		//Debug.Log (gameObject.GetComponents<Material> ());
+		foreach (Renderer renderer in renderers) {
+			Material[] mats = renderer.materials;
+			for(int i = 0; i < mats.Length; i++){
+				Debug.Log ("RENDERER? "+mats[i]);
+				mats[i] = lilMat;
+			}
+			renderer.materials = mats;
+		}
 	}
 
 	// @TODO: Make this disentegrate it like the portal fizzler instead.
--- Assets/Scripts/PartZone.cs
+++ Assets/Scripts/PartZone.cs
@@ -1,23 +1,63 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class PartZone : MonoBehaviour {
 	public BurgBuilder bb;
 	new public bool enabled = true;
-
+	
+	public Dictionary<GameObject, bool> collisions = new Dictionary<GameObject, bool>();
+	
 	// Use this for initialization
 	void Start () {
 		//bb = transform.parent.GetComponent<BurgBuilder> ();
 	}
 
-	void OnCollisionEnter(Collision col){
-		if (enabled) {
-			if (col.gameObject.tag == "Food") {
-				//Debug.Log (col.gameObject);
-				col.gameObject.GetComponent<Food> ().isFoodPope = true;
-				bb.ObtainNewPart (col.gameObject);
-				enabled = false;
+	void Update(){
+		List<GameObject> keys = new List<GameObject>(collisions.Keys);
+		foreach (GameObject key in keys){
+			if(collisions[key] && ShouldObtainObject (key)){
+				ForceObtain(key);
 			}
 		}
+	}
+	
+	void OnCollisionEnter(Collision col){
+		Debug.Log ("PLATE FOUND " + col.gameObject);
+		collisions.Add (col.gameObject, true);
+		if (ShouldObtainObject(col.gameObject)) {
+			//Debug.Log ("GRANTING NEW OBJECT: "+col.gameObject);
+			//isFoodPope = false;
+			ForceObtain(col.gameObject);
+		}
+	}
+	
+	void OnCollisionExit(Collision col){
+		Debug.Log ("PLATE DIDN'T WANT " + col.gameObject);
+		collisions.Remove (col.gameObject);
+	}
+	
+	public bool ShouldObtainObject(GameObject obj){
+		if (enabled) {
+			Debug.Log ("SOO: WAS ENABLED");
+		}
+		if (obj.tag == "Food") {
+			Debug.Log ("SOO: WAS FOOD");
+		}
+		if (!obj.GetComponent<Food> ().isGrabbed) {
+			Debug.Log ("SOO: WAS NOT GRABBED");
+		}
+		if (enabled && obj.tag == "Food" && !obj.GetComponent<Food> ().isGrabbed) {
+			Debug.Log ("ALLOWED " + obj);
+			return true;
+		} else {
+			Debug.Log ("REJECTED " + obj);
+			return false;
+		}
+	}
+
+	public void ForceObtain(GameObject obj){
+		collisions [obj] = false;
+		bb.ObtainNewPart (obj);
 	}
 }
