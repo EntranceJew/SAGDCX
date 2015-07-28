755fadf39ab983d51cd88e06922bf74b1256c6ae
EntranceJew <EntranceJew@gmail.com>
2015-07-28 14:37:31 -0400

    Fixed NullRatferenceExceptions

--- Assets/Scripts/RatMachine.cs
+++ Assets/Scripts/RatMachine.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 
 public class RatMachine : MonoBehaviour {
 	public List<GameObject> spawnList;
-	public Inventory inventory;
 	public DayManager dayManager;
 	
 	private int listPos;
@@ -12,18 +11,15 @@ public class RatMachine : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		listPos = 0;
-		if (inventory == null) {
-			inventory = PlayerValues.pv.ratsInHouse;
-		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (dayManager.isDayActive) {
 			foreach (RatThing plate in this.gameObject.GetComponentsInChildren<RatThing>()) {
-				if (inventory.Has (spawnList [listPos].name)) {
+				if (PlayerValues.pv.ratsInHouse.Has (spawnList [listPos].name)) {
 					if (plate.SpawnThing (spawnList [listPos])) {
-						inventory.Remove (spawnList [listPos].name, 1);
+						PlayerValues.pv.ratsInHouse.Remove (spawnList [listPos].name, 1);
 						listPos += 1;
 					}
 				} else {
